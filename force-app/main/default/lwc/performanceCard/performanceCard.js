96730bed658270e80e47252d78e727f2ca357fb0
nkottcamp <nskottcamp@gmail.com>
2023-07-13 16:29:28 -0600

    Made some changes to the way the RelatedListComp and performanceCard work to attempt to make this LWC community/exp site friendly. using custom apex wire now instead of lightning adapter to bypass 404 error which might be related to known issue.

--- force-app/main/default/lwc/performanceCard/performanceCard.js
+++ force-app/main/default/lwc/performanceCard/performanceCard.js
@@ -1,37 +1,37 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import { NavigationMixin } from 'lightning/navigation';
-
-const FIELDS = [
-    'Account.Name',
-    'Account.Phone',
-    'Account.Description'
-];
+import getAccounts from '@salesforce/apex/festivalsLWCHandler.getAccounts';
+// const FIELDS = [
+//     'Account.Name',
+//     'Account.Phone',
+//     'Account.Description'
+// ];
 
 export default class FestivalCard extends NavigationMixin(LightningElement){
     @api recId; 
+    accountResults;
+    accountRecords;
+    accountErrors;
+    wireApexResultsActs;
+
     
-    @wire(getRecord, { recordId: '$recId', fields: FIELDS })
-    account;
-
-    get name() {
-        //console.log('16')
-        return this.account.data.fields.Name.value;
+    @wire(getAccounts, {recordId: '$recId'})
+    wiredActs(accountResults) {
+        console.log('20')
+        this.wireApexResultsActs = accountResults;
+        if(accountResults.data){
+            console.log('21')
+            this.accountRecords = accountResults.data;
+            console.log(accountResults.data);
+            this.accountErrors = undefined;
+        }
+        if(accountResults.error){
+            this.accountRecords = undefined;
+            this.accountErrors = accountResults.error;
+        }
     }
-
-    get phone() {
-        //console.log('17')
-        return this.account.data.fields.Phone.value;
-    }
-
-    get description() {
-        //console.log('18')
-        return this.account.data.fields.Description.value;
-    }
-
-    connectedCallback() {
-        //console.log('15');
-    }
+    
 
     handleClick() {
         this[NavigationMixin.GenerateUrl]({ 
@@ -44,6 +44,32 @@ export default class FestivalCard extends NavigationMixin(LightningElement){
          }).then((url) => {
             window.open(url);
          });
+    }
+
+    // @wire(getRecord, { recordId: '$recId', fields: FIELDS, optionalFields: FIELDS })
+    // account;
+
+    // get name() {
+    //     console.log('16')
+    //     return this.account.data.fields.Name.value;
+    // }
+
+    // get phone() {
+    //     console.log('17')
+    //     return this.account.data.fields.Phone.value;
+    // }
+
+    // get description() {
+    //     console.log('18')
+    //     return this.account.data.fields.Description.value;
+    // }
+
+    connectedCallback() {
+        console.log('15');
+    }
+
+    renderCallBack() {
+        console.log('19');
     }
 
 
